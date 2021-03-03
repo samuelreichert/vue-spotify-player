fe58d2064b1ee7bdd7e35502805a299f2f61efc2
Samuel Reichert <samuelreichertt@gmail.com>
2021-03-03 10:10:57 -0300

    Create a config file with the relevant data to connect with spotify api

---
+++ config.js
@@ -0,0 +1,8 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize'
+export const clientId = '1a4f8f6ff64d486f939c566c79dc56c4'
+export const redirectUri = 'http://localhost:3000/redirect'
+export const scopes = [
+  'user-top-read',
+  'user-read-currently-playing',
+  'user-read-playback-state',
+]
